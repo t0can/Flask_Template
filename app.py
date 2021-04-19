2968979081a8dbb1b342ee7ff3b56ea9a24e4073
Fletcher Le Sueur <53324493+t0can@users.noreply.github.com>
2021-04-19 11:05:51 +1000

    Final

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-print("hello git")
+from flask import Flask
+
+app = Flask(__name__)
+
+template = 'temp.html'
+
+
+@app.route('/main')
+def home():
+    return template
+
+
+if __name__ == '__main__':
+    app.run()
